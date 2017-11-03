b5182f87f7d2e2bc14ed5947545484f48fce33d4
tam94131 <del@tahl.net>
2017-11-03 12:06:32 -0700

    sort show data from backend

--- src/PublicProfile.js
+++ src/PublicProfile.js
@@ -6,14 +6,12 @@ class PublicProfile extends Component{
       <div>
         <div id="profilePage">
           <div className="container">
-            <img src="https://www.proflowers.com/blog/wp-content/uploads/2014/03/tulip-thumbnail-350x240.jpg" width="100" alt="profile-img"/>
-            <h2>Austin</h2>
-            <p>Current City: San Francisco</p>
-            <div className="postSummary">
-              <p>Here are my posts! </p>
-              <p>Here are my posts! </p>
-              <p>Here are my posts! </p>
-            </div>
+
+          <img id="profile-avatar" width="100" alt="profile-img" />
+          <h2><span id="profile-name"></span></h2>
+          <p>Current City: <span id="profile-city"></span></p>
+          <div className="postSummary"></div>
+
           </div>
         </div>
       </div>
