da3a5b60665e12dd1901589c2e815bdd61d7372f
snopeks <creativesnopeks@gmail.com>
2017-11-04 23:38:50 -0700

    update cities bootstrap

--- src/Cities.js
+++ src/Cities.js
@@ -32,8 +32,9 @@ class Cities extends Component {
         <div id="cityPage">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-sm-5">
-                <h2>Cities</h2>
+
+              <div className="col-sm-5 cityRow">
+              <h2>Cities</h2>
                 <br></br>
                 <div id="cityTarget">
                   <div className="btn-group">
@@ -41,9 +42,15 @@ class Cities extends Component {
                     <div className="city">
                       <div className="row">
                         <div className="col-md-12">
+<<<<<<< 4124827113f5ca7ee665bc9541daefad5caa4ecc
                           <a type="button" name="sfClick">
                             <img src={this.state.citiesArray[0].img} className="cityListImg" alt="SF" width="200" height="100"/>
                            {this.state.citiesArray[0].name}
+=======
+                          <a type="button">
+                            <img src="img/San Francisco.jpg" className="cityListImg" alt="San Francisco" width="100" height="50"/>
+                            San Francisco
+>>>>>>> update cities bootstrap
                           </a>
                         </div>
                       </div>
@@ -53,8 +60,13 @@ class Cities extends Component {
                     <div className="city">
                       <div className="row">
                         <div className="col-md-12">
+<<<<<<< 4124827113f5ca7ee665bc9541daefad5caa4ecc
                           <a type="button" name="nyClick">
                             <img src="img/New York.jpeg" className="cityListImg" alt="New York" width="200" height="100"/>
+=======
+                          <a type="button">
+                            <img src="img/New York.jpeg" className="cityListImg" alt="New York" width="100" height="50"/>
+>>>>>>> update cities bootstrap
                             New York
                           </a>
                         </div>
@@ -65,8 +77,13 @@ class Cities extends Component {
                     <div className="city">
                       <div className="row">
                         <div className="col-md-12">
+<<<<<<< 4124827113f5ca7ee665bc9541daefad5caa4ecc
                           <a type="button" name="vcClick">
                             <img src="img/Vancouver.jpg" className="cityListImg" alt="Vancouver" width="200" height="100"/>
+=======
+                          <a type="button">
+                            <img src="img/Vancouver.jpg" className="cityListImg" alt="Vancouver" width="100" height="50"/>
+>>>>>>> update cities bootstrap
                             Vancouver
                           </a>
                         </div>
@@ -76,7 +93,7 @@ class Cities extends Component {
                 </div>
                 <br></br>
               </div>
-              <div className="col-sm-6">
+              <div className="col-sm-7">
                 <div className="row">
                   <div id="selectedCity">
 
@@ -90,8 +107,6 @@ class Cities extends Component {
                     </div>
                   </div>
                 </div>
-
-                  <h2>City Posts</h2>
 
                   <PostList />
 
